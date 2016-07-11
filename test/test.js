64c94b69ff8cd21bc4f9b71471a3ad885243a0b7
pishon <szybkistart24@gmail.com>
2016-07-11 17:57:32 +0200

    test with mocha introduced

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('app describe', function() {
+  it('assertion will be done', function() {    
+    expect(true).to.equal(true);
+  });
+});
